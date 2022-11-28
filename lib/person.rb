4a701596db901228ae96859bd7f03476b3612355
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-11-28 07:20:14 -0800

    commit tree 2042db35cafd696e401632aa6ff7cc6e689a7a16

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
+require "pry"
+
 class Person
-  # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
+# binding.pry
+
+# bob_attributes = { name: "Bob", hair_color: "Brown" }
+
+# bob = Person.new(bob_attributes)
